926b00876be2d9db39538ee25e8f9b00931069a0
Talic72 <EMORRISWORK72@GMAIL.COM>
2026-03-22 13:49:50 -0400

    Intitial setup and workout Controller

---
+++ Controllers/WorkoutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LiftingApp.Controllers
+{
+    public class WorkoutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Workout page working as expectted");
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LiftingApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
